775f3907b51d9a90e135a405b1bb443afde6a1cd
January Ansa <dimbayaa@januaryansa.com>
2017-07-26 14:13:22 -0500

    adding untracked js files

---
+++ js/gj.js
@@ -0,0 +1,9 @@
+//General javascript
+$('.container').imagesLoaded( function() {
+  // images have loaded
+  $('body').addClass('loaded');
+});
+// imagesLoaded( '.container.main', { background: true }, function() {
+//
+//   console.log('.container background image loaded');
+// });
---
+++ js/ups_validate.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+var inputs= document.querySelectorAll('.valid');
+
+var ups_data =array[
+  {
+      "AccessRequest": {
+        "AccessLicenseNumber":
+        "Your Access License Number",
+        "UserId": "Your Username",
+        "Password": "Your Password"
+      },
+    "AddressValidationRequest": {
+      "Request": {
+        "TransactionReference": {
+         "CustomerContext":
+         "Your Customer Context",
+          },
+         "RequestAction": "AV"
+       },
+      "Address": { 
+        "City": "ALPHARETTA",
+        "StateProvinceCode": "GA",
+        "PostalCode": "30005"
+      }
+    }
+  }
+];
+
+});
