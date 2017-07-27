bf520ea798d4da58acd8ff8fe6bfb307b013db22
January Ansa <dimbayaa@januaryansa.com>
2017-07-27 09:29:59 -0500

    small updated the secure ssl url for registration page

--- js/gj.js
+++ js/gj.js
@@ -3,7 +3,3 @@ $('.container').imagesLoaded( function() {
   // images have loaded
   $('body').addClass('loaded');
 });
-// imagesLoaded( '.container.main', { background: true }, function() {
-//
-//   console.log('.container background image loaded');
-// });
